054044935ac6133870011ebee5c8882bb9211ec9
lairmartes <lairmartes@gmail.com>
2018-01-27 12:05:54 -0200

    Including javadoc information in Disk and refactoring for Generics in compareTo(T)

--- src/com/gmail/lairmartes/game/hanoitower/Disk.java
+++ src/com/gmail/lairmartes/game/hanoitower/Disk.java
@@ -1,36 +1,74 @@
 /*
- * Criado em 26/09/2005
- *
- * Para alterar o gabarito para este arquivo gerado vá para
- * Janela&gt;Preferências&gt;Java&gt;Geração de Códigos&gt;Código e Comentários
+ * Created in 09/26/2005
+ * Refactored in 01/18/2018
  */
 package com.gmail.lairmartes.game.hanoitower;
 
-/**
+/** Disk element is designed to be put in Pin.
+ *  The Disk element has a size to be compared with another disks.
+ *  In Hanoi Tower game, a greater disk can't be put above a lesser disk.
+ *
+ * @see Pin
  * @author Lair Martes Junior - 2CCPN - USJT
  *
  */
-public class Disk implements Comparable{
+public class Disk implements Comparable<Disk>{
+	/** DISK_ZERO is the Null Disk object that is used to avoid NullPointerException.
+	 */
 	public final static Disk DISK_ZERO = new Disk(0);
 	private int _size;
 	private String _toString;
+
+	/**
+	 * A Disk element must be constructed with a size.
+	 *
+	 * @param size
+	 */
 	public Disk(int size) {
 		_size = size;
 		_toString = "disk sizing " + size;
 	}
+
+	/** Returns the size of the disk.
+	 *
+	 * @return the size of the disk.
+	 */
 	public int getSize() {
 		return _size;
 	}
-	public int compareTo(Object o) {
-		if (!(o instanceof Disk)) return 0;
-		return _size - ((Disk)o).getSize();
+
+	/** Returns the size of the object minus the size of the compared object it.
+	 * For example:
+	 * 	- if both elements have the same size, it returns zero.
+	 * 	- if the current element has size 10 and compared element has size 4, the result is 6 (positive).
+	 * 	- if the current element has size 3 and compared element has size 11, the result is -8 (negative).
+	 *  <b>Warning: </b> if the compared object isn't an instance of Disk, the result is 0.
+	 *
+	 *  @override Comparable.compareTo
+	 *
+	 * @param aDisk a Disk to be compared with the current disk.
+	 * @return the value obtained from the operation of this element size minus the compared element size.
+	 */
+	public int compareTo(Disk aDisk) {
+		return _size - aDisk.getSize();
 	}
+
+	/** Returns a string containing the size of this Disk.
+	 *
+	 * @override Object.toString
+	 * @return a string with the size of the disk
+	 */
 	public String toString() {
 		return _toString;
 	}
-	public boolean equals(Object o) {
-		if (!(o instanceof Disk))
-			return false;
-		return ((Disk)o).getSize() == this.getSize();
+
+	/** Returns true if the current disk has the same size of the given disk.
+	 *
+	 * @override Object.equals
+	 * @param aDisk Disk to be compared.
+	 * @return true if the disks have the same size.
+	 */
+	public boolean equals(Disk aDisk) {
+		return aDisk.getSize() == this.getSize();
 	}
 }
